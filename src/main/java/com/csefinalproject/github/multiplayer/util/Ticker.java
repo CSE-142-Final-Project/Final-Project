79f75fdaff7e875392a91695b2c8502249463d82
RogueFirework <atticus.pak@gmail.com>
2023-05-19 14:59:27 -0700

    Add ticker stuff

--- src/main/java/com/csefinalproject/github/multiplayer/util/Ticker.java
+++ src/main/java/com/csefinalproject/github/multiplayer/util/Ticker.java
@@ -1,5 +1,22 @@
 package com.csefinalproject.github.multiplayer.util;
 
 public class Ticker {
+    public Ticker(int TPS) {
 
+    }
+
+    /**
+     * Runs this function every tick
+     * @param toRun Function to run
+     */
+    public void subscribe(Runnable toRun) {
+
+    }
+
+    public void start() {
+
+    }
+    public void stop() {
+
+    }
 }
