328547837a46714d75e54d2c94bb799cefa78fe0
Jayden <113476107+ilhs1096069@users.noreply.github.com>
2023-05-30 15:00:03 -0700

    Start removing bad stuff

--- src/main/java/com/csefinalproject/github/multiplayer/Main.java
+++ src/main/java/com/csefinalproject/github/multiplayer/Main.java
@@ -1,10 +1,10 @@
 package com.csefinalproject.github.multiplayer;
 
-import com.csefinalproject.github.multiplayer.networking.server.Server;
 import org.apache.commons.cli.*;
 import com.csefinalproject.github.multiplayer.behaviour.client.ClientManager;
 import com.csefinalproject.github.multiplayer.behaviour.server.GameManager;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -49,54 +49,7 @@ class CommandLineResponseGetter {
 			throw new RuntimeException(e);
 		}
 
-		// Get program type
-		if (commandLine.hasOption("s")) {
-			this.programType = "server";
-		} else if (commandLine.hasOption("c")) {
-			this.programType = "client";
-		} else {
-			String response = GetResponse(console,
-					"Do you want to be a (S)erver or a (C)lient? ",
-					"Answer must be (S) for Server or (C) for Client.",
-					new String[] {
-							"s", "server",
-							"c", "client"
-					}
-			);
-
-			if(response.equals("s")) {
-				this.programType = "server";
-			} else if (response.equals("c")) {
-				this.programType = "client";
-			} else {
-				this.programType = response;
-			}
-		}
-
-		// Get IP and Port
-		if (commandLine.hasOption("i")) {
-			this.ip = commandLine.getOptionValue("i");
-		} else {
-			String response = GetResponse(console,
-					"Give the IP of the " + programType + " you want to connect to. ",
-					"",
-					new String[] { }
-			);
-
-			this.ip = response;
-		}
-
-		if (commandLine.hasOption("p")) {
-			this.port = Short.parseShort(commandLine.getOptionValue("p"));
-		} else {
-			String response = GetResponse(console,
-					"Give the port of the " + programType + " you want to connect to. ",
-					"",
-					new String[] { }
-			);
-
-			this.port = Short.parseShort(response);
-		}
+		System.out.println();
 	}
 
 	/**
@@ -104,7 +57,7 @@ class CommandLineResponseGetter {
 	 * @param console The System.in scanner object.
 	 * @return "client" or "server"
 	 */
-	private static String GetResponse(Scanner console, String prompt, String errorMessage, String[] correctResponses) {
+	private String GetResponse(Scanner console, String prompt, String errorMessage, String[] correctResponses) {
 		// Might get stuck in permanent loop! Yay!
 		while(true) {
 			System.out.print(prompt);
@@ -129,34 +82,29 @@ class CommandLineResponseGetter {
 	 * Create the command line options
 	 * @return Server and Client command line options
 	 */
-	private static Options CreateCommandLineOptions() {
+	private Options CreateCommandLineOptions() {
 		Options options = new Options();
 
-		options.addOption(Option.builder("i")
+		options.addOption(Option.builder("ip")
 				.required(false)
 				.desc("IP to connect to.")
-				.longOpt("ip")
 				.build()
-		);
-		options.addOption(Option.builder("p")
+		).addOption(Option.builder("p")
 				.required(false)
 				.desc("Port to open on/connect to.")
 				.longOpt("p")
 				.build()
-		);
-
-		options.addOption(Option.builder("s")
+		).addOption(Option.builder("s")
 				.required(false)
 				.desc("Starts a server")
 				.longOpt("server")
 				.build()
-		);
-		options.addOption(Option.builder("c")
+		).addOption(Option.builder("c")
 				.required(false)
 				.desc("Starts a client")
 				.longOpt("client")
 				.build()
-		);
+		);;
 
 		return options;
 	}
